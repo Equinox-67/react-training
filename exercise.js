f064e98b3921183b6306f8107db7579e93b72f27
Equinox-67 <EQU2005HAS@GMAIL.COM>
2023-07-29 14:22:09 +0100

    Assignment commit

---
+++ exercise.js
@@ -0,0 +1,36 @@
+const statuses = ["system_enabled", "system_disabled", "customer_enabled", "customer_disabled"];
+
+const result = [];
+
+statuses.forEach(function (status, index, object) {
+  status = `${object.slice(1,2)}, ${object.slice(3, 4)}`;
+  result.push(status);
+})
+
+console.log(result)
+
+// Assignment 2
+
+let studentAndScores = [
+  {firstName: 'John',   lastName: 'Doe',     score: 23},
+  {firstName: 'Jane',   lastName: 'Akinola', score: 79},
+  {firstName: 'Basit',   lastName: 'Aina',    score: 15},
+  {firstName: 'Olamide', lastName: 'Oguntan', score: 65},
+  {firstName: 'Sanni',   lastName: 'Tijani',  score: 65},
+]
+
+const studentName = studentAndScores.map(function callback(item) {
+  if (item.score <= 40) {
+    item.score = 'D';
+  } else if(item.score <= 60) {
+    item.score = 'C';
+  } else if(item.score <= 80) {
+    item.score = 'B';
+  } else if(item.score <= 100) {
+    item.score = 'A';
+  }
+  let fullName = `${item.firstName} ${item.lastName},  ${item.score}`;
+  return fullName;
+})
+
+console.log(studentName);
---
+++ index.js
@@ -0,0 +1,40 @@
+// let names = ["Julius", "Juliet", "Comfort", "Obedience"];
+
+// let namesWithAge = [];
+
+// for (let name of names) {
+//   const newName = `${name}, 34`;
+//   namesWithAge.push(newName);
+// }
+// console.log(namesWithAge);
+
+
+// Arrow function
+
+// let items =[2, 3, 4];
+
+// const resulta = items.map(function callback(item) {
+//   const squaredValue = item ** 2;
+//   return squaredValue;
+// });
+
+// console.log(resulta);
+
+// const namesa = [['John', 'Doe'],['Jane', 'Doe'],['Abdulfatai', 'Aka']]
+
+// const resultb = namesa.map(function callback(item) {
+//   const fullname = `${item[0]} ${item[1]}`;
+//   return fullname
+// })
+
+// console.log(resultb)
+
+// const meter = [2000,4000,60000,3000];
+
+// const result = meter.map(function callback(item) {
+//   const kiloMeter = item/1000;
+//   return kiloMeter;
+// })
+
+// console.log(result);
+
